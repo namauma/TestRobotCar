ad51f9f296cbb24a5dd2888700c321a37ae99442
namauma <ikuma@gm.ishikawa-nct.ac.jp>
2026-07-13 03:41:34 +0000

    first extension

---
+++ main.ts
@@ -0,0 +1,11 @@
+//% color=#ff6600 icon="\uf1b9"
+namespace robotcar {
+
+    /**
+     * テスト
+     */
+    //% block="こんにちは"
+    export function hello(): void {
+        basic.showString("HELLO")
+    }
+}
